79b79b7de6d4fecf654a524156f1d14de819ebee
zachnkck <90000333+zachnkck@users.noreply.github.com>
2022-08-06 15:07:30 -0500

    Groundwork for xp calculator

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 # Dark Heresy 2E - Continued
 
-An **UNOFFICIAL** system for playing Dark Heresy 2E on [Foundry VTT](https://foundryvtt.com/).
+An (unoffially unofficial update to an) **UNOFFICIAL** system for playing Dark Heresy 2E on [Foundry VTT](https://foundryvtt.com/).
 
 It provides support for **character sheets only**, game content should be drawn from official source books.
 
 The project is being continued under the GPL-3.0 License after the original author deleted their project.
+^^ This fork is an experimental continuation to moo-man (et al.)'s continuation. It's mostly tweaked for private use and mostly consists of some specific features bolted on.
 
-## Install
-1. Go to the setup page and choose **Game Systems**.
-2. Click the **Install System** button, and paste in this [manifest link](https://raw.githubusercontent.com/moo-man/DarkHeresy2E-FoundryVTT/master/system.json)
-3. Create a Game World using the Dark Heresy system.
 
 ## Preview
 ![Acolyte 1](https://raw.githubusercontent.com/moo-man/DarkHeresy2E-FoundryVTT/master/asset/preview/acolyte1.jpg)  
---
+++ script/common/xpCosts.js
@@ -0,0 +1,280 @@
+var skillCosts = [
+	[300, 600, 900, 1200], 
+	[200, 400, 600, 800], 
+	[100, 200, 300, 400]
+];		//ex. for 0 matching aptitudes, use index[0][x]
+
+var charCosts = [
+	[500, 750, 1000, 1500, 2500], 
+	[250, 500, 750, 1000, 1500], 
+	[100, 250, 500, 750, 1250]
+];		//same as above
+
+
+
+
+var skillAptitudes = [
+	{
+		name: "Acrobatics",
+		aptitudes: [
+			"Agility",
+			"General"
+		]
+	},
+	{
+		name: "Athletics",
+		aptitudes: [
+			"Strength",
+			"General"
+		]
+	},
+	{
+		name: "Awareness",
+		aptitudes: [
+			"Perception",
+			"Fieldcraft"
+		]
+	},
+	{
+		name: "Charm",
+		aptitudes: [
+			"Fellowship",
+			"Social"
+		]
+	},
+	{
+		name: "Command",
+		aptitudes: [
+			"Fellowship",
+			"Leadership"
+		]
+	},
+	{
+		name: "Commerce",
+		aptitudes: [
+			"Intelligence",
+			"Knowledge"
+		]
+	},
+	{
+		name: "Common Lore",
+		aptitudes: [
+			"Intelligence",
+			"General"
+		]
+	},
+	{
+		name: "Decieve",
+		aptitudes: [
+			"Fellowship",
+			"Social"
+		]
+	},
+	{
+		name: "Dodge",
+		aptitudes: [
+			"Agility",
+			"Defense"
+		]
+	},
+	{
+		name: "Forbidden Lore",
+		aptitudes: [
+			"Intelligence",
+			"Knowledge"
+		]
+	},
+	{
+		name: "Inquiry",
+		aptitudes: [
+			"Fellowship",
+			"Social"
+		]
+	},
+	{
+		name: "Interrogation",
+		aptitudes: [
+			"Willpower",
+			"Social"
+		]
+	},
+	{
+		name: "Intimidate",
+		aptitudes: [
+			"Strength",
+			"Social"
+		]
+	},
+	{
+		name: "Linguistics",
+		aptitudes: [
+			"Intelligence",
+			"General"
+		]
+	},
+	{
+		name: "Logic",
+		aptitudes: [
+			"Intelligence",
+			"Knowledge"
+		]
+	},
+	{
+		name: "Medicae",
+		aptitudes: [
+			"Intelligence",
+			"Fieldcraft"
+		]
+	},
+	{
+		name: "Navigate",
+		aptitudes: [
+			"Intelligence",
+			"Fieldcraft"
+		]
+	},
+	{
+		name: "Operate",
+		aptitudes: [
+			"Agility",
+			"Fieldcraft"
+		]
+	},
+	{
+		name: "Parry",
+		aptitudes: [
+			"Weapon Skill",
+			"Defense"
+		]
+	},
+	{
+		name: "Psyniscience",
+		aptitudes: [
+			"Perception",
+			"Psyker"
+		]
+	},
+	{
+		name: "Scholastic Lore",
+		aptitudes: [
+			"Intelligence",
+			"Knowledge"
+		]
+	},
+	{
+		name: "Scrutiny",
+		aptitudes: [
+			"Perception",
+			"General"
+		]
+	},
+	{
+		name: "Security",
+		aptitudes: [
+			"Intelligence",
+			"Tech"
+		]
+	},
+	{
+		name: "Sleight of Hand",
+		aptitudes: [
+			"Agility",
+			"Knowledge"
+		]
+	},
+	{
+		name: "Stealth",
+		aptitudes: [
+			"Agility",
+			"Fieldcraft"
+		]
+	},
+	{
+		name: "Survival",
+		aptitudes: [
+			"Perception",
+			"Fieldcraft"
+		]
+	},
+	{
+		name: "Tech-Use",
+		aptitudes: [
+			"Intelligence",
+			"Tech"
+		]
+	},
+	{
+		name: "Trade",
+		aptitudes: [
+			"Intelligence",
+			"General"
+		]
+	}
+];
+
+var charAptitudes = [
+	{
+		name: "Weapon Skill",
+		aptitudes: [
+			"Weapon Skill",
+			"Offence"
+		]
+	},
+	{
+		name: "Ballistic Skill",
+		aptitudes: [
+			"Ballistic Skill",
+			"Finesse"
+		]
+	},
+	{
+		name: "Strength",
+		aptitudes: [
+			"Strength",
+			"Offence"
+		]
+	},
+	{
+		name: "Toughness",
+		aptitudes: [
+			"Toughness",
+			"Defense"
+		]
+	},
+	{
+		name: "Agility",
+		aptitudes: [
+			"Agility",
+			"Finesse"
+		]
+	},
+	{
+		name: "Intelligence",
+		aptitudes: [
+			"Intelligence",
+			"Knowledge"
+		]
+	},
+	{
+		name: "Perception",
+		aptitudes: [
+			"Perception",
+			"Fieldcraft"
+		]
+	},
+	{
+		name: "Willpower",
+		aptitudes: [
+			"Willpower",
+			"Psyker"
+		]
+	},
+	{
+		name: "Fellowship",
+		aptitudes: [
+			"Fellowship",
+			"Social"
+		]
+	}
+];
+
